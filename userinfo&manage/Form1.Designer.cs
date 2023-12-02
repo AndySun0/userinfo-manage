db45312c51c7dd07d3576e856ca13cdd4ff0eab9
AndySun0 <syx2003year@gmail.com>
2023-12-02 11:09:36 +0800

    添加项目文件。

---
+++ userinfo&manage/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace userinfo_manage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            time = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(295, 78);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // time
+            // 
+            time.Location = new Point(143, 167);
+            time.Name = "time";
+            time.Size = new Size(504, 30);
+            time.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1578, 844);
+            Controls.Add(time);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Userinfo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox time;
+    }
+}
---
+++ userinfo&manage/Form1.cs
@@ -0,0 +1,16 @@
+namespace userinfo_manage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string timestr = DateTime.Now.ToString();
+            time.Text = timestr;
+        }
+    }
+}
